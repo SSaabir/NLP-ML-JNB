bc40e142c1e3750ff763e50d1177970214a7fe4f
Saabir <siraajsaabir@gmail.com>
2025-09-06 03:22:06 +0530

    Add initial notebook for report generation and SQL audit log for Colombo weather data queries
    - Created a new Jupyter notebook file `report_agent.ipynb` for future report generation tasks.
    - Added an SQL audit log `sql_audit.log` containing a query to retrieve weather data for Colombo over the last 7 days.

--- Multi-label-Classification/backend/main.py
+++ Multi-label-Classification/backend/main.py
@@ -18,7 +18,7 @@ class Features(BaseModel):
     title: str
     overview: str
 
-#model = joblib.load("")
+model = joblib.load("../best_model.pkl")
 
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')   
 
@@ -27,12 +27,11 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/predict")
-#data: Features
-def predict():
-    return {"result": "Hello World!"}
+def predict(data: Features):
     combined_text = f"{data.title} {data.overview}"
-    embeddings = embedding_model.encode([combined_text])
-    prediction = model.predict(embeddings)
-    
+    prediction = model.predict([combined_text])  # pass as a list of strings
+    return {"result": prediction.tolist()}
 
-#{"result": prediction.tolist()}
+
+
+#
